cf5d57e632ddcf520aadaf8dd43baac512952112
jermany17 <leejm5050@naver.com>
2026-08-05 23:45:23 +0900

    오류 해결

--- backend/src/test/java/com/back/BackendApplicationTests.java
+++ backend/src/test/java/com/back/BackendApplicationTests.java
@@ -4,8 +4,10 @@ import com.back.global.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestContainersConfig.class)
 class BackendApplicationTests {
 
