244f11fbb2d5a6faac825c3cb4141659428e1c6a
Jeongmin Lim <simount3@naver.com>
2026-09-08 14:58:13 +0900

    livekit 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("io.livekit:livekit-server:0.13.0") // 채팅방 입장 토큰 발급, 방 제어
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
